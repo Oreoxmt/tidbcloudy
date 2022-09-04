f072d1a3d7c4f9e31e13715352a715143687f3b7
Aolin <aolin.zhang@pingcap.com>
2022-09-05 00:02:58 +0800

    add examples

---
+++ examples/0_list_resources.py
@@ -0,0 +1,30 @@
+import os
+
+import tidbcloudy
+from tidbcloudy.project import Project
+
+public_key = os.environ.get("PUBLIC_KEY")
+private_key = os.environ.get("PRIVATE_KEY")
+debug_mode = os.environ.get("TIDBCLOUDY_LOG")
+project_id = "1234567890123456789"
+
+api = tidbcloudy.TiDBCloud(public_key=public_key, private_key=private_key)
+
+for project in api.iter_projects():
+    assert isinstance(project, Project)
+    print(project)
+    for cluster in project.iter_clusters():
+        print(cluster)
+        if cluster.cluster_type.value == "DEDICATED":
+            for backup in cluster.iter_backups():
+                print(backup)
+    for restore in project.iter_restores():
+        print(restore)
+
+# get_project() does not fetch full information of a project from the server by default
+project = api.get_project(project_id)
+print(project)
+
+# To fetch full information of a project, set update_from_server as True
+project = api.get_project(project_id, update_from_server=True)
+print(project)
---
+++ examples/3_connect_mysql.py
@@ -0,0 +1,24 @@
+import os
+import tidbcloudy
+
+public_key = os.environ.get("PUBLIC_KEY")
+private_key = os.environ.get("PRIVATE_KEY")
+debug_mode = os.environ.get("TIDBCLOUDY_LOG")
+project_id = "1234567890123456789"
+cluster_id = "1234567890123456789"
+
+print("Connecting to TiDB Cloud...")
+api = tidbcloudy.TiDBCloud(public_key=public_key, private_key=private_key)
+project = api.get_project(project_id, update_from_server=True)
+cluster = project.get_cluster(cluster_id)
+print(cluster)
+
+if cluster.status.cluster_status.value == "AVAILABLE":
+    connection_strings = cluster.status.connection_strings
+    connection = cluster.connect(type="standard", database="test", password="your_root_password")
+    print(connection)
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT DATABASE();")
+            m = cursor.fetchone()
+            print(m[0])
