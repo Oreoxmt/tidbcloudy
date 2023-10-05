6dbef1b68bdaeeb241a8e25fef2671e39c0f13fe
Aolin <aolinz@outlook.com>
2023-10-05 21:15:36 +0800

    feat: support specifying timezone in timestamp_to_string() and get_current_year_month

--- tidbcloudy/util/timestamp.py
+++ tidbcloudy/util/timestamp.py
@@ -1,26 +1,31 @@
-import datetime
+from datetime import datetime, tzinfo
+from typing import Union
 
 
-def timestamp_to_string(timestamp: int) -> str:
+def timestamp_to_string(timestamp: Union[int, None], timezone: tzinfo = None) -> str:
     """
     Convert timestamp to datetime string.
     Args:
-        timestamp:
+        timestamp: the timestamp to convert.
+        timezone: the timezone to use.
 
     Returns:
-        the datetime string.
+        the datetime string in format of YYYY-MM-DD HH:MM:SS.
 
     """
     if timestamp is None:
         return ""
-    return datetime.datetime.fromtimestamp(timestamp).isoformat()
+
+    return datetime.fromtimestamp(timestamp, tz=timezone).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def get_current_year_month() -> str:
+def get_current_year_month(timezone: tzinfo = None) -> str:
     """
     Get current year and month.
+    Args:
+        timezone: the timezone to use.
     Returns:
         the year and month string in format of YYYY-MM.
 
     """
-    return datetime.datetime.now().strftime("%Y-%m")
+    return datetime.now(tz=timezone).strftime("%Y-%m")
