e4b2af4f751d293fb8f3d3f22811af46a673ce2f
Aolin <aolinz@outlook.com>
2023-10-14 15:16:09 +0800

    refactor: get the IPv4 address of the client using Cloudflare cdn-cgi/trace

--- tidbcloudy/util/ip.py
+++ tidbcloudy/util/ip.py
@@ -4,11 +4,16 @@ import httpx
 
 
 def get_current_ip_address() -> str:
-    host = "ifconfig.co"
+    host = "oreo.life"
+    # Get the IPv4 address of the host
     addrs = socket.getaddrinfo(host, 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP)
     for addr in addrs:
         ipv4_addr = addr[4][0]
-        resp = httpx.get("http://" + ipv4_addr + "/ip", headers={"Host": host})
+        # Request the IPv4 address of the client from the host
+        resp = httpx.get(f"http://{ipv4_addr}/cdn-cgi/trace", headers={"Host": host})
         resp.raise_for_status()
-        return resp.text.strip()
+        for line in resp.text.splitlines():
+            if line.startswith("ip="):
+                # Return the IPv4 address of the client
+                return line[3:]
     raise Exception("Failed to get the current IP address")
